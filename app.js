34bfde5066ba064362606c87b3b3aa290fed0e8b
cgilmore112 <calebtgil@gmail.com>
2022-04-11 12:21:44 -0500

    updated

--- app.js
+++ app.js
@@ -3,6 +3,63 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var Food = require("./models/foods");
+
+const connectionString =  
+process.env.MONGO_CON 
+mongoose = require('mongoose'); 
+mongoose.connect(connectionString,  
+{useNewUrlParser: true, 
+useUnifiedTopology: true}); 
+
+// We can seed the collection if needed on server start 
+async function recreateDB(){ 
+  // Delete everything 
+  await Food.deleteMany(); 
+ 
+  let instance1 = new 
+Food({category:"fruit",  quality:'good', 
+cost:4}); 
+  instance1.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("First object saved") 
+  }); 
+} 
+ 
+let reseed = true; 
+if (reseed) { recreateDB();}
+
+async function recreateDB(){ 
+  // Delete everything 
+  await Food.deleteMany(); 
+ 
+  let instance2 = new 
+Food({category:"veggie",  quality:'poor', 
+cost:1}); 
+  instance2.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("Second object saved") 
+  }); 
+} 
+ 
+reseed = true; 
+if (reseed) { recreateDB();} 
+
+async function recreateDB(){ 
+  // Delete everything 
+  await Food.deleteMany(); 
+ 
+  let instance3 = new 
+Food({category:"fruit",  quality:'good', 
+cost:4}); 
+  instance3.save( function(err,doc) { 
+      if(err) return console.error(err); 
+      console.log("Third object saved") 
+  }); 
+} 
+ 
+reseed = true; 
+if (reseed) { recreateDB();} 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
